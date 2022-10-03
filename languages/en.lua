31a0382d666f3f36e085ef6c0f3d0c6e1d9ca67f
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-10-03 22:33:11 +0200

    Create en.lua

---
+++ languages/en.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------------
+---------------------------------- ENGLISH -------------------------------------------
+
+Locales["de"] = {
+
+  prompt = "Press",
+  alreadySearched = "It seems someone has already searched this",
+  found = "You found something and took it",
+  nothingFound = "You didn't find anything",
+  searching= "Searching..."
+ 
+}
